13962816f3b40560df45179db71e9521779ec25a
Li He <jet.in.brain@gmail.com>
2014-02-22 23:22:40 +0800

    project initialized...

---
+++ src/com/doomdagger/jextractor/support/TagSelectorPair.java
@@ -0,0 +1,35 @@
+package com.doomdagger.jextractor.support;
+
+
+public class TagSelectorPair {
+	
+	private String key;
+	private String value;
+	private String extra;
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public String getExtra() {
+		return extra;
+	}
+	public void setExtra(String extra) {
+		this.extra = extra;
+	}
+	public TagSelectorPair(String key, String value, String extra) {
+		super();
+		this.key = key;
+		this.value = value;
+		this.extra = extra;
+	}
+	public TagSelectorPair() {}
+	
+}
