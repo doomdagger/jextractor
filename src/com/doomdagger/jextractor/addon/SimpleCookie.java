ccea518a1389fc57b114cacfe8ccbb64a6c0e287
Li He <jet.in.brain@gmail.com>
2014-02-24 14:06:32 +0800

    add module: FetchUrl client. Simply Encapsulate HttpClient from Apache Foundation.

---
+++ src/com/doomdagger/jextractor/addon/SimpleCookie.java
@@ -0,0 +1,90 @@
+package com.doomdagger.jextractor.addon;
+
+import java.util.Date;
+
+import org.apache.http.cookie.Cookie;
+
+public class SimpleCookie implements Cookie{
+	private String name;
+	private String value;
+	
+	public SimpleCookie(){
+		
+	}
+	
+	public SimpleCookie(String name, String value){
+		this.name = name;
+		this.value = value;
+	}
+	
+	@Override
+	public String getComment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCommentURL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDomain() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Date getExpiryDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String getPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] getPorts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getValue() {
+		return value;
+	}
+
+	@Override
+	public int getVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isExpired(Date arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isPersistent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isSecure() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/com/doomdagger/jextractor/addon/URLFetcher.java
@@ -0,0 +1,128 @@
+package com.doomdagger.jextractor.addon;
+
+import static com.doomdagger.jextractor.Extractor.*;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+//请使用IP地址，节约DNS查询时间
+public class URLFetcher {	
+		
+	public static FetchResponse fetchURLByGet(String url, String sessionId){
+		//System.err.println("GET in session:"+sessionId);
+		
+		
+		FetchResponse fetchResponse = new FetchResponse();
+		
+		
+		url = repairURL(url);
+		
+		//创建sdk对象
+		
+		JExtractorFetchUrl fetch= new JExtractorFetchUrl();
+		//打开允许重定向开关,默认
+		fetch.setAllowRedirect(false);
+		//设置最大重定向次数
+		fetch.setRedirectNum(0);
+		//设置cookie
+		fetch.setCookie("JSESSIONID", sessionId);
+		//发起一次get请求
+		fetchResponse.setResponseBody(fetch.get(url));
+		//获取http code
+		fetchResponse.setStatusCode(fetch.getStatusCode());
+		//获取返回的头部信息
+		fetchResponse.setResponseHeader(fetch.getResponseHeaders());
+				
+		//如果非登陆，响应报文不会给Cookie的，这时候直接用方法传进来的cookie赋值给FetchResponse，如果为登陆，手动获取
+		if(fetch.getResponseCookies()!=null){
+			Pattern pattern = Pattern.compile(".*?JSESSIONID=(.*?);.*");
+			Matcher matcher = pattern.matcher(fetch.getResponseCookies());
+			if(matcher.find()){
+				fetchResponse.setSessionId(matcher.group(1));
+			}
+		}
+		
+		if(fetchResponse.getSessionId()==null||fetchResponse.getSessionId().equals("")){
+			fetchResponse.setSessionId(sessionId);
+		}
+		
+		//System.err.println("GET OUT session:"+fetchResponse.getSessionId());
+		//System.err.println(fetchResponse.getResponseHeader().toString());
+		
+		return fetchResponse;
+	}
+	
+	//推荐使用MapMaker一行搞定初始化一个map，示例  MapMaker.instance("j_username", "1018110323").param("j_password", "******").toMap()
+	public static FetchResponse fetchURLByPost(String url, String sessionId, Map<String, String> params){
+		//System.err.println("POST in session:"+sessionId);
+
+		
+		FetchResponse fetchResponse = new FetchResponse();
+		
+		url = repairURL(url);
+		
+		//创建sdk对象
+		JExtractorFetchUrl fetch= new JExtractorFetchUrl();
+		//打开允许重定向开关,默认
+		fetch.setAllowRedirect(false);
+		//设置最大重定向次数
+		fetch.setRedirectNum(0);
+		//设置cookie
+		fetch.setCookie("JSESSIONID", sessionId);		
+		//设置post参数，实际post的内容
+		fetch.setPostData(params);		
+		//发起一次post请求
+		fetchResponse.setResponseBody(fetch.post(url));
+		//获取头部信息
+		fetchResponse.setResponseHeader(fetch.getResponseHeaders());
+		//获取http code
+		fetchResponse.setStatusCode(fetch.getStatusCode());
+		
+		//如果非登陆，响应报文不会给Cookie的，这时候直接用方法传进来的cookie赋值给FetchResponse，如果为登陆，手动获取
+		if(fetch.getResponseCookies()!=null){
+			Pattern pattern = Pattern.compile(".*?JSESSIONID=(.*?);.*");
+			Matcher matcher = pattern.matcher(fetch.getResponseCookies());
+			if(matcher.find()){
+				fetchResponse.setSessionId(matcher.group(1));
+			}
+		}
+		
+		if(fetchResponse.getSessionId()==null||fetchResponse.getSessionId().equals("")){
+			fetchResponse.setSessionId(sessionId);
+		}
+		
+		
+		//System.err.println("POST OUT session:"+fetchResponse.getSessionId());
+		//System.err.println(fetchResponse.getResponseHeader().toString());
+		
+		return fetchResponse;
+	}
+	
+	private static String repairURL(String url){
+		if(!url.startsWith("http://"))
+			return "http://"+url;
+		return url;
+	}
+	
+	public static void main(String[] args){
+		long currentTime = System.currentTimeMillis();
+		FetchResponse response = fetchURLByPost("202.199.128.21/academic/j_acegi_security_check", null, MapMaker.instance("j_username", "1018110323").param("j_password", "lh911119").toMap());
+		System.err.println((System.currentTimeMillis()-currentTime)/(double)1000+"s");
+		currentTime = System.currentTimeMillis();
+		FetchResponse response2 = fetchURLByGet("202.199.128.21/academic/showHeader.do", response.getSessionId());
+		System.err.println((System.currentTimeMillis()-currentTime)/(double)1000+"s");
+
+		System.out.println(response.getSessionId());
+		
+		if(response2.getStatusCode()!=200){
+			System.out.println("登陆失败或已经超时，重新登陆！");
+		}else {
+			
+			String infoString = $("#greeting", response2.getResponseBody()).get(0).getText().trim();
+			
+			System.out.println(infoString);
+		}
+	}
+}
